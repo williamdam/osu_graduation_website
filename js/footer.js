47357ba779b700f56b82da96b365e4b45b4e1354
William Dam <damw@oregonstate.edu>
2021-04-13 12:16:39 -0700

    Footer info update.

--- js/footer.js
+++ js/footer.js
@@ -3,7 +3,7 @@ footerBar.setAttribute("class", "osu-footer");
 
 var footerContent = document.createElement("p");
 footerContent.setAttribute("class", "text-light container no-margin-bottom");
-footerContent.innerHTML = "&copy; William Dam - OSU Analyst Programmer Candidate | (971) 377-8211 | willdam@gmail.com";
+footerContent.innerHTML = "&copy; William Dam - Software Engineer | (949) 478-1931 | willdam@gmail.com";
 footerBar.appendChild(footerContent);
 
 document.body.appendChild(footerBar);
